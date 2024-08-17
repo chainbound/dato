3c094c2979df474601b0e93e29ebb62a9d54c989
Naman Garg <0708ng@gmail.com>
2024-08-16 21:31:20 -0700

    feat: read msg api

--- src/client/api.rs
+++ src/client/api.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use alloy::primitives::Bytes;
+use alloy::primitives::{Bytes, B256};
 use axum::{
     extract::{Query, State},
     http::StatusCode,
@@ -10,19 +10,24 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 
-use crate::{primitives::Request, CertifiedLog, CertifiedRecord, Log, Timestamp};
+use crate::{
+    primitives::Request, CertifiedLog, CertifiedReadMessageResponse, CertifiedRecord, Log,
+    Timestamp,
+};
 
 use super::{Client, ClientSpec};
 
 const WRITE_PATH: &str = "/api/v1/write";
 const READ_PATH: &str = "/api/v1/read";
 const READ_CERTIFIED_PATH: &str = "/api/v1/read_certified";
+const READ_MESSAGE_PATH: &str = "/api/v1/read_message";
 
 pub async fn run_api(client: Client, port: u16) -> eyre::Result<()> {
     let router: Router = Router::new()
         .route(WRITE_PATH, post(write))
         .route(READ_PATH, get(read))
         .route(READ_CERTIFIED_PATH, get(read_certified))
+        .route(READ_MESSAGE_PATH, get(read_message))
         .with_state(Arc::new(client));
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await?;
@@ -93,6 +98,27 @@ async fn read_certified(
 
     client
         .read_certified(namespace, params.start.into(), params.end.into())
+        .await
+        .map(Json)
+        .map_err(|e| StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+#[derive(Debug, Deserialize)]
+struct ReadMessageParams {
+    namespace: String,
+    msg_id: B256,
+}
+
+#[tracing::instrument(skip(client, params))]
+async fn read_message(
+    State(client): State<Arc<Client>>,
+    params: Query<ReadMessageParams>,
+) -> Result<Json<CertifiedReadMessageResponse>, StatusCode> {
+    let namespace = Bytes::from(params.namespace.as_bytes().to_owned());
+    tracing::debug!("New read_message request for namespace: {namespace}");
+
+    client
+        .read_message(namespace, params.msg_id)
         .await
         .map(Json)
         .map_err(|e| StatusCode::INTERNAL_SERVER_ERROR)
