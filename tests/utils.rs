58971344db9cd14cdd515866b4ab2dc22b6ba473
Naman Garg <0708ng@gmail.com>
2024-08-22 00:41:15 +0530

    chore(test): take port as input

--- tests/utils.rs
+++ tests/utils.rs
@@ -3,10 +3,10 @@ use std::net::SocketAddr;
 use blst::min_pk::PublicKey as BlsPublicKey;
 use dato::{bls::random_bls_secret, Validator};
 
-pub async fn spin_up_validator() -> eyre::Result<(SocketAddr, BlsPublicKey)> {
+pub async fn spin_up_validator(port: Option<u16>) -> eyre::Result<(SocketAddr, BlsPublicKey)> {
     let dummy_sk = random_bls_secret();
     let pubkey = dummy_sk.sk_to_pk();
-    let validator = Validator::new_in_memory(dummy_sk, 0).await?;
+    let validator = Validator::new_in_memory(dummy_sk, port.unwrap_or(0)).await?;
     let validator_addr = validator.local_addr().expect("Listening");
     tokio::spawn(validator);
 
